273c0bed4f43152cdb4e34ddc602fd74bcaecf62
Jack Bennetto <jack@bennetto.com>
2018-12-26 21:15:48 -0800

    add kde, alternate interface, minor changes to multihist

--- tools.py
+++ tools.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from scipy import stats
 import matplotlib.pyplot as plt
 
 '''
@@ -10,20 +11,32 @@ TODO
   call as list of arrays
 '''
 
-
-def histpair(x, y=None,
-             bins=None, binsize=None,
-             xmin=None, xmax=None, ymax=None,
-             normed=1, alpha=0.2, figsize=(12, 8), title=None,
-             ax=None):
+def multihist(x, y=None,
+              bins=None, binsize=None,
+              xmin=None, xmax=None, top=None,
+              kde=None,
+              density=True, alpha=0.2, figsize=(12, 8), title=None,
+              ax=None):
     '''
     INPUT:
     x:      numpy array; point of a distribution
     y:      numpy array of the same length; will plot histogram
-            for each unique value of y
+            for each unique value of y.
+            If None (the default), x should be a list of arrays;
+            a histogram with be created for each, labeled sequentially.
     bins:   int; number of bins in histogram
-    xmin:   lower limit; other
-    normed: normalize number of elements in each class
+    binsize:float: size of bins (overrides bins)
+ 
+    xmin:   lower limit (or None to set to min of data)
+    xmax:   upper limit (or None to set to max of data)
+    top:    upper limit of y
+
+    density: normalize number of elements in each class
+    kde:    add kde plot
+    alpha:  float; opacity; pass to matplotlib
+    figsize:tuple; width and height of figure; pass to matplotlib
+    title:  str; title of plot
+    ax:     axis on which to plot histograms
     '''
     if ax is None:
         fig, ax = plt.subplots(figsize=figsize)
@@ -46,15 +59,27 @@ def histpair(x, y=None,
                          xc.max() + binsize,
                          binsize)
 
-    for yval in np.unique(y):
-        ax.hist(list(xc[y == yval]),
-                alpha=alpha,
-                bins=binarray,
-                normed=normed,
-                label=str(yval))
+    # We need to get the default color cycle to get the same color
+    # for the hist and kde line.
+    props = plt.rcParams['axes.prop_cycle']
+
+    for yval, prop in zip(np.unique(y), props):
+        color = prop['color']
+
+        h = ax.hist(list(xc[y == yval]),
+                    alpha=alpha,
+                    bins=binarray,
+                    density=density,
+                    label=str(yval),
+                    color=color)
+        if kde:
+            kde_func = stats.gaussian_kde(xc[y == yval])
+            if not density:
+                kde_scale = np.sum(y == yval) * binsize
+            ax.plot(xvals, kde_scale * kde_func(xvals), color=color)
+        
     ax.set_xlim(xmin=xmin, xmax=xmax)
-    if ymax is not None:
-        ax.set_ylim(0, ymax)
+    ax.set_ylim(top=top)
     ax.legend()
 
 
