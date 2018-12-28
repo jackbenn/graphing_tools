3bb8b25d7e23c245a70ed76fa74882a04a8f4ef9
Jack Bennetto <jack@bennetto.com>
2018-12-27 16:30:23 -0800

    add confusion-matrix code

--- tools.py
+++ tools.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from scipy import stats
 import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
 
 '''
 TODO
@@ -116,3 +117,66 @@ def axdline(ax, slope, intercept, **kwargs):
     ax.set_xlim(xlim)
     ax.set_ylim(ylim)
     return line[0]
+
+
+def plot_confusion_matrix(ax,
+                          y_true,
+                          y_pred,
+                          grid=False,
+                          area=True,
+                          normalization="maximum"):
+    """
+    Parameters
+    ----------
+    ax : matplotlib axis on which to draw graph.
+    y_true : list-like object
+        actual labels.
+    y_pred : list-like object of the same size as y_true
+        predictions based on model.
+    grid : bool, default False
+        whether to draw a grid.
+    area : bool, default: True
+        if True, the area of the square is proportional to the value. If False, the length
+        of a side is used.
+    nornalization : string, default: 'maximum'
+        How to normalize the values.
+        'maximum' : normalize all values so the largest value is 1.
+        'prediction' or 'precision' : normalize by columns so that the sum of each prediction is 1,
+            so the values represent the precisions
+        'true' or 'recall': normalize by rows so that the sum of each true value is 1,
+            so the values represent the recalls
+    """
+    cm = confusion_matrix(y_true, y_pred).astype(float).transpose()
+    if normalization == 'maximum':
+        cm /= cm.max()
+    elif normalization == 'prediction' or normalization == 'precision':
+        cm /= cm.sum(axis=1, keepdims=True)
+    elif normalization == 'true' or normalization == 'recall':
+        cm /= cm.sum(axis=0, keepdims=True)
+    else:
+        raise ValueError("`normalization` should be one of `all`, `true`, or `predictions`")
+    n = cm.shape[0]
+    
+    labels = np.unique(y_true)
+    tics = np.arange(n+1)
+    for axis in [ax.xaxis, ax.yaxis]:
+        axis.set_ticks(tics + 0.5, minor=True)
+        axis.set(ticks=tics, ticklabels=labels)
+    ax.tick_params(axis='both', which='major', length=0)
+
+    ax.set_xlim(-0.5, n-0.5)
+    ax.set_ylim(-0.5, n-0.5)
+
+    ax.invert_yaxis()
+    ax.grid(grid, which='minor')
+    
+    ax.set_xlabel('prediction')
+    ax.set_ylabel('true')
+
+    if area:
+        cm **= 0.5
+    for i in range(n):
+        for j in range(n):
+            size = cm[i, j]
+            square = matplotlib.patches.Rectangle((i - size/2, j - size/2), size, size)
+            ax.add_patch(square)
